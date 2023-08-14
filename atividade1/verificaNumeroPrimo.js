1650dc9d47e0d22611c4a63735661fc256c1432e
pedrolodonio <pedrolodonio20@gmail.com>
2023-08-14 19:15:54 -0300

    atividade 1

---
+++ atividade1/verificaNumeroPrimo.js
@@ -0,0 +1,14 @@
+let num = 6
+
+for (let c = 2; c < num; c++) {
+
+    if (num % c == 0) {
+        console.log("0")
+        break
+    }
+    
+    else{
+        console.log("1")
+        break
+    }
+}
