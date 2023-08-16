561c4483696cebd2c6ec10afc72f2d6bdcba144c
pedrolodonio <pedrolodonio20@gmail.com>
2023-08-16 10:38:48 -0300

    adicionando atividade2

---
+++ atividade2/exercicio1.js
@@ -0,0 +1,18 @@
+let x = 8
+let y = 2
+let z
+
+console.log("Operações")
+
+z = x + y
+console.log(`${x} + ${y} = ${z}`)
+
+z = x - y
+console.log(`${x} - ${y} = ${z}`)
+
+z = x * y
+console.log(`${x} * ${y} = ${z}`)
+
+z = x / y
+console.log(`${x} / ${y} = ${z}`)
+
